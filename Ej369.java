219fb1542da0f7bb8982994172a012e2da740336
LaraCabido <94174162+LaraCabido@users.noreply.github.com>
2021-11-12 13:06:17 +0100

    Update Ej369.java

--- Ej369.java
+++ Ej369.java
@@ -1,15 +1,15 @@
-
 package aceptaelreto2;
 
 import java.util.Scanner;
 
 public class Ej369 {
+
     public static void main(String[] args) {
         int a, suma;
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
-        while (a != 0){
-            for (int i = 0; i < a; i++){
+        while (a != 0) {
+            for (int i = 0; i < a; i++) {
                 System.out.print("1");
             }
             System.out.println();
